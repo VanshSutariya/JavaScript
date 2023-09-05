9389ff45d513d8765fc642ad5dd971feed71caf8
Vansh Sutariya <sutariyavansh@gmail.com>
2023-09-05 19:44:09 +0530

    First Commit

---
+++ tut2.js
@@ -0,0 +1,10 @@
+console.time('Your code Took');
+console.log ('Hello Console');
+console.log(4+34);
+console.log([2,4,59,6,5]);
+console.log({harry: 'this', marks:54});
+console.table({harry: 'this', marks:54});
+console.warn(' this is warning');
+console.timeEnd('Your code Took');
+//console.assert(2<189 , 'age <189 is exits');
+// console.error('this is an error')
---
+++ tut3.js
@@ -0,0 +1,22 @@
+// const , var 
+console.log('tut3');
+var name = `vansh`;
+name ='xsr';
+var channel;
+var marks = 100;
+channel = `sutariya`;
+console.log(name , channel, marks);
+const iname = 'Raj';
+//iname='rou';
+console.log(iname);
+{
+ let gcity ='surat';
+ console.log(gcity);
+}
+/*var arr=[45,87,2,6,4];
+arr.push=[1,6]
+console.log(arr);
+*/
+
+const arr=[45,87,2,6,4];
+console.log(arr);
